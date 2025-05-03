ed2f5f64efb7e141addf39e58b48a198dda3ecf7
gaston <gaston.gutierrez.9964359@usalesiana.edu.bo>
2025-05-02 22:43:08 -0400

    create the enpoint for to see a specifict news

--- dist/main.js
+++ dist/main.js
@@ -7,7 +7,7 @@ const swagger_1 = require("@nestjs/swagger");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors({
-        origin: 'http://localhost:3001',
+        origin: '*',
         methods: 'GET,POST,PATCH,DELETE',
     });
     const config = new swagger_1.DocumentBuilder()
--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: '*',
     methods: 'GET,POST,PATCH,DELETE',
   });
   
-  
-
 
     const config = new DocumentBuilder()
     .setTitle('MacNews API')
